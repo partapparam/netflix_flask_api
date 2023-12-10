38e1359d59a8502f9564debecd2042bcf8514a46
partapparam <paramsingh1961@gmail.com>
2023-12-10 12:45:31 -0800

    defined a content by type route

--- api/__init__.py
+++ api/__init__.py
@@ -6,17 +6,32 @@ from api.models import Content
 def create_app(database, user):
     app = Flask(__name__)
 
-    app.config.from_mapping(DATABASE=database, USER=user)
+    app.config.from_mapping(
+        DATABASE=database, 
+        USER=user)
+    
+    conn = psycopg2.connect(database=app.config['DATABASE'], user=app.config['USER'])
+        # cursor = conn.cursor()
 
     @app.route('/')
     def content_list():
         query = 'SELECT * FROM content LIMIT 10;'
-        conn = psycopg2.connect(database=app.config['database'], user=app.config['user'])
+        # conn = psycopg2.connect(database=app.config['DATABASE'], user=app.config['USER'])
         cursor = conn.cursor()
         cursor.execute(query)
         content = cursor.fetchall()
         content_objs = [Content(row).__dict__ for row in content]
         return content_objs
     
+    @app.route('/<type>')
+    def content_by_type(type):
+        query = """SELECT * FROM content
+                WHERE type = %s"""
+        cursor = conn.cursor()
+        type = type.upper()
+        cursor.execute(query, (type,))
+        content = cursor.fetchall()
+        content_objs = [Content(row).__dict__ for row in content]
+        return content_objs
 
     return app
---
+++ manage.py
@@ -0,0 +1,7 @@
+from settings import DATABASE, USER
+from api import create_app
+
+app = create_app(database=DATABASE, user=USER)
+
+if __name__ == '__main__':
+    app.run()
